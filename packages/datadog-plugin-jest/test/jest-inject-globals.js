fcac2bf38a7867c0d28688899ecb76785fd2e88a
Leandro Dornelles <leandrosdornelles@gmail.com>
2023-08-16 11:28:33 -0300

    Ajuste require jest globals

--- packages/datadog-plugin-jest/test/jest-inject-globals.js
+++ packages/datadog-plugin-jest/test/jest-inject-globals.js
@@ -1,5 +1,3 @@
-const { describe, it, expect } = require('../../../versions/@jest/globals').get()
-
 describe('jest-inject-globals', () => {
   it('will be run', () => {
     expect(true).toEqual(true)
